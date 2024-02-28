3d2b41f54c54bfb0f85ce182409088b1667c87a7
= <andy.froberg@gmail.com>
2024-02-27 21:55:07 -0800

    progress

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ from fastapi.templating import Jinja2Templates
 
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="staic")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/")
 async def name(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request, "logged_in": False})
+    page_title = "My Birthday Reminder"
+    logged_in = False
+    return templates.TemplateResponse(request=request, name="home.html", context={"page_title": page_title, "logged_in": logged_in})
 
 @app.get("/my-reminders")
 async def reminders(request: Request):
